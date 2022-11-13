14ba80b3ed4a687b4884809dffe813d1ddc084e3
Bartlomiej Pedryc <b.pedryc@poczta.fm>
2022-11-13 19:27:05 +0100

    Remove class active from all articles

--- js/script.js
+++ js/script.js
@@ -10,10 +10,20 @@
     console.log(event);
 
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
 
@@ -25,5 +35,5 @@
   const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+        link.addEventListener('click', titleClickHandler);
   }
